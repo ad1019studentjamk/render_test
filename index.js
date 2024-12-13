2c95cdf69e3e4a76f6b67424b5ad987d64fa2560
akseli kettula <ad1019@student.jamk.fi>
2024-12-13 16:03:41 +0200

    added a hardcoded mock database for testing purposes

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ app.use(
 // app.use(express.json());
 app.use(cors());
 
+let persons = [
+  { id: '1', name: 'Arto Hellas', number: '040-123456' },
+  { id: '2', name: 'Ada Lovelace', number: '39-44-5323523' },
+  { id: '3', name: 'Dan Abramov', number: '12-43-234345' },
+  { id: '4', name: 'Mary Poppendieck', number: '39-23-6423122' },
+];
+
 app.get('/', (request, response) => {
   response.send('<p>hello world</p>');
 });
